39b0997a4c44f69809ffacae615b4fdb77ce096b
nsbaee <znxpsxk8@navere.com>
2020-08-12 14:39:54 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Hello from './Hello';
+import Wrapper from './Wrapper';
 
 import './App.css';
 
 function App() {
   return (
    <>
+   <Wrapper>
    <Hello name="나" color="red"/>
    <Hello color="blue"/>
+   </Wrapper>
    </>
   );
 }
---
+++ src/Wrapper.js
@@ -0,0 +1,18 @@
+P
+
+import React from 'react'
+
+function Wrapper ({childen}){
+    const style={
+        border: '2px solid black',
+        padding: '16px'
+
+    }
+    return(
+        <div style={style}>
+            {childen}
+        </div>
+    )
+}
+
+export default Wrapper;
