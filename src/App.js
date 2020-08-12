cca8040a65e8b0172a384cddea4cb74b83319af4
nsbaee <znxpsxk8@navere.com>
2020-08-12 14:52:35 +0900

    first

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Wrapper from './Wrapper';
 
 import './App.css';
 
+//조건부 랜더링
+
 function App() {
   return (
    <>
    <Wrapper>
-   <Hello name="나" color="red"/>
+   <Hello name="나" color="red" isSpecial={true}/>
    <Hello color="blue"/>
    </Wrapper>
    </>
